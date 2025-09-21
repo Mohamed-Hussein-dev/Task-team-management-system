5e8d4657bfdb287ec15e0532b79d2419ef6d0eb4
Mohamed-Hussein-Dev <mohamd.h.yousuf@gmail.com>
2025-09-21 20:34:02 +0300

    feat(project): Add ProjectsController with POST endpoint

---
+++ src/TaskTeamManagementSystem.Api/Controllers/ProjectController.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using System.Security.Claims;
+using TaskTeamManagementSystem.Api.Helper;
+using TaskTeamManagementSystem.Application.Features.Projects.Commands;
+using TaskTeamManagementSystem.Application.Features.Projects.Commands.DTOs;
+
+namespace TaskTeamManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ProjectController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> CreateProject(CreateProjectDto project)
+        {
+            var UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var command = new CreateProjectCommand(project , UserId);
+
+            var res = await mediator.Send(command);
+
+            if (res.Success) {
+                return Ok(res);
+            }
+            return BadRequest(res);
+        }
+    }
+}
