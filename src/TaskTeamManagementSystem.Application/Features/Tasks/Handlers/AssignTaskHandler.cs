7da1b164ab1fb73299b78ff6c593b0fca73f1f11
Mohamed-Hussein-Dev <mohamd.h.yousuf@gmail.com>
2025-09-22 23:46:17 +0300

    feat(task): impelement assaign task logic

---
+++ src/TaskTeamManagementSystem.Application/Features/Tasks/Handlers/AssignTaskHandler.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTeamManagementSystem.Application.Features.Common.Responses;
+using TaskTeamManagementSystem.Application.Features.Tasks.Commands;
+using TaskTeamManagementSystem.Application.Interfaces;
+using TaskTeamManagementSystem.Domain.Entities.Identtity;
+
+namespace TaskTeamManagementSystem.Application.Features.Tasks.Handlers
+{
+    public class AssignTaskHandler : IRequestHandler<AssignTaskCommand, BaseResponse<bool>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UserManager<AppUser> userManager;
+
+        public AssignTaskHandler(IUnitOfWork unitOfWork , UserManager<AppUser> userManager)
+        {
+            this.unitOfWork = unitOfWork;
+            this.userManager = userManager;
+        }
+        public async Task<BaseResponse<bool>> Handle(AssignTaskCommand request, CancellationToken cancellationToken)
+        {
+            var User = await userManager.FindByEmailAsync(request.UserEmail);
+
+            if (User == null) {
+                return BaseResponse<bool>.Fail("User not exist");
+            }
+
+            var Task = await unitOfWork.Tasks.GetByIdAsync(request.TaskId);
+
+            if (Task == null) {
+                return BaseResponse<bool>.Fail("Task not exist");
+            }
+
+            Task.AssigneeUserId = User.Id;
+
+            var AssignTask = await unitOfWork.CommitChangesAsync();
+
+            if(AssignTask == 0)
+                return BaseResponse<bool>.Fail("Assign Task was Failed");
+            
+            return BaseResponse<bool>.Ok(true, "Task Assigned Correctly");
+        }
+    }
+}
