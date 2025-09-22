2a22932ed8fd54f30b6e2e2535887c64e59e979b
Mohamed-Hussein-Dev <mohamd.h.yousuf@gmail.com>
2025-09-22 17:36:46 +0300

    feat(fix) : add Project Id property

--- src/TaskTeamManagementSystem.Application/Features/Tasks/DTOs/UpdateTaskDto.cs
+++ src/TaskTeamManagementSystem.Application/Features/Tasks/DTOs/UpdateTaskDto.cs
@@ -9,6 +9,7 @@ namespace TaskTeamManagementSystem.Application.Features.Tasks.DTOs
     public class UpdateTaskDto
     {
         public int TaskId { get; set; }
+        public int ProjectId {  get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
