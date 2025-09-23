d7db4ef7501116ffca7af715a6ef4d690f09a303
Mohamed-Hussein-Dev <mohamd.h.yousuf@gmail.com>
2025-09-23 14:30:20 +0300

    feat(project): add DTOs for project managment

---
+++ src/TaskTeamManagementSystem.Application/Features/Projects/DTOs/UpdateProjectDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTeamManagementSystem.Application.Features.Projects.DTOs
+{
+    public class UpdateProjectDto
+    {
+        public int ProjectId { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
